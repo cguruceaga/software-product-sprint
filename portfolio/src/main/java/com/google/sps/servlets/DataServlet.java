589da77b5795f2c8f018e0f0014f0ecb722fb6dd
Carlota Guruceaga <carlotaguruceaga@yahoo.fr>
2020-06-13 13:33:47 -0400

    part 3 tutorial add hardcoded comments using json

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -19,14 +19,36 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import com.google.gson.Gson;
+
+
 
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
+    ArrayList<String> messages = new ArrayList<String>();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+       
+    messages.add("hello this is a comment");
+    messages.add("another comment!");
+    messages.add("comment number 3");
+
     response.setContentType("text/html;");
-    response.getWriter().println("Hello Carlota");
+
+    String json = convertToJsonUsingGson(messages);
+
+    response.getWriter().println(json);
   }
+
+  //json file
+    private String convertToJsonUsingGson(ArrayList<String> messages) {
+        Gson gson = new Gson();
+        String json = gson.toJson(messages);
+        return json;
+    }
 }
+
